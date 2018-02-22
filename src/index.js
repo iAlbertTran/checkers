fc7f8a55a5481f2eee31b84209e755068ce5cda9
Albert Tran <albtran@ucdavis.edu>
2018-02-22 03:04:55 -0800

    changed how legal moves are calculated to minimize and optimize code required, as well as cut down on hard code

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ function Square(props){
 					value={props.value}
 				/>
 			</button> :
-			<button className={className} onClick={props.onClick}></button>
+			<button className={className}></button>
 	);
 }
 
@@ -122,7 +122,6 @@ class Game extends React.Component{
  	}
 
 	handleClick(square){
-
 		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
@@ -239,258 +238,109 @@ ReactDOM.render(<Game />, document.getElementById('root'));
 
 
 function legalMove(square, movingPiece, turn, squares, jump){
-	const blackAllowed = [
-		[0, null, 9],
-		[2, 9, 11],
-		[4, 11, 13],
-		[6, 13, 15],
-		[9, 16, 18],
-		[11, 18, 20],
-		[13, 20, 22],
-		[15, 22, null],
-		[16, null, 25],
-		[18, 25, 27],
-		[20, 27, 29],
-		[22, 29, 31],
-		[25, 32, 34],
-		[27, 34, 36],
-		[29, 36, 38],
-		[31, 38, null],
-		[32, null, 41],
-		[34, 41, 43],
-		[36, 43, 45],
-		[38, 45, 47],
-		[41, 48, 50],
-		[43, 50, 52],
-		[45, 52, 54],
-		[47, 54, null],
-		[48, null, 57],
-		[50, 57, 59],
-		[52, 59, 61],
-		[54, 61, 63]
-	];
 
-	const redAllowed = [
-		[63, 54, null],
-		[61, 52, 54],
-		[59, 50, 52],
-		[57, 48, 50],
-		[54, 45, 47],
-		[52, 43, 45],
-		[50, 41, 43],
-		[48, null, 41],
-		[47, 38, null],
-		[45, 36, 38],
-		[43, 34, 36],
-		[41, 32, 34],
-		[38, 29, 31],
-		[36, 27, 29],
-		[34, 25, 27],
-		[32, null, 25],
-		[31, 22, null],
-		[29, 20, 22],
-		[27, 18, 20],
-		[25, 16, 18],
-		[22, 13, 15],
-		[20, 11, 13],
-		[18, 9, 11],
-		[16, null, 9],
-		[15, 6, null],
-		[13, 4, 6],
-		[11, 2, 4],
-		[9, 0, 2]
-	];
+	//destination square must be empty, otherwise can't move there
+	if(squares[square])
+		return [null, null];
 
-	const blackOverRed = [
-		[0, null, 18],
-		[2, 16, 20],
-		[4, 18, 22],
-		[6, 20, null],
-		[9, null, 27],
-		[11, 25, 29],
-		[13, 27, 31],
-		[15, 29, null],
-		[16, null, 34],
-		[18, 32, 36],
-		[20, 34, 38],
-		[22, 36, null],
-		[25, null, 43],
-		[27, 41, 45],
-		[29, 43, 47],
-		[31, 45, null],
-		[32, null, 50],
-		[34, 48, 52],
-		[36, 50, 54],
-		[38, 52, null],
-		[41, null, 59],
-		[43, 57, 61],
-		[45, 59, 63],
-		[47, 61, null],
-	];
+	const leftEdge = [0, 16, 32, 48];
+	const rightEdge = [15, 31, 47, 63];
 
-	const redOverBlack = [
-		[63, 45, null],
-		[61, 43, 47],
-		[59, 41, 45],
-		[57, null, 43],
-		[54, 36, null],
-		[52, 34, 38],
-		[50, 32, 36],
-		[48, null, 34],
-		[47, 29, null],
-		[45, 27, 31],
-		[43, 25, 29],
-		[41, null, 27],
-		[38, 20, null],
-		[36, 18, 22],
-		[34, 16, 20],
-		[32, null, 18],
-		[31, 13, null],
-		[29, 11, 15],
-		[27, 9, 13],
-		[25, null, 11],
-		[22, 4, null],
-		[20, 2, 6],
-		[18, 0, 4],
-		[16, null, 2],
-	];
+	const redPieces = ['X', 'XK'];
+	const blackPieces = ['O', 'OK'];
 
-	if (turn === 'O'){
-		for (let i = 0; i < blackAllowed.length; ++i){
-			const [moving, left, right] = blackAllowed[i];
-			if(moving === movingPiece && ((left === square && squares[left] === null) || (right === square && squares[right] === null) )){
-				if (jump)
-					return [null, null];
-				return [true, null];
-			}
-		}
 
-		for (let i = 0; i < blackOverRed.length; ++i){
-			const [moving, left, right] = blackOverRed[i];
-			if(moving === movingPiece){
+	//left and right move. If moving piece is X, moveA = right move, moveB = left move, and vice versa for O
+	let moveA = 7;
+	let moveB = 9;
 
-				if 	(left === square && (squares[left - 7] === "X" || squares[left - 7] === 'XK') && squares[left] === null)
-						return [true, left - 7];
-
-				if 	(right === square && (squares[right - 9] === "X" || squares[right - 9] === 'XK') && squares[right] === null)
-						return [true, right - 9]; 
-			}
-		}
+	//if jump is being attempted, moveA and moveB are doubled;
+	if(square - movingPiece > 10 || movingPiece - square > 10){
+		moveA = 14;
+		moveB = 18;
 	}
 
-	else if (turn === 'X'){
-		for (let i = 0; i < redAllowed.length; ++i){
-			const [moving, left, right] = redAllowed[i];
-			if(moving === movingPiece && ((left === square && squares[left] === null) || (right === square && squares[right] === null)) ){
-				if (jump)
-					return [null, null];
+	let possibleMoves;
+	//generate a list of possible moves depending on what kind of piece is moving
+	switch (turn){
 
-				return [true, null];
-			}
+		case 'X':
+			possibleMoves = [movingPiece - moveB, movingPiece - moveA, null, null];
+			break;
+
+		case 'O':
+			possibleMoves = [null, null, movingPiece + moveA, movingPiece + moveB];
+			break;
+
+		case 'XK':
+		case 'OK':
+			possibleMoves = [movingPiece - moveB, movingPiece - moveA, movingPiece + moveA, movingPiece + moveB];
+			break;
+	}
+
+	//corresponds to the space in between the moving piece and its potential destination. Used to check if it has the opponent piece occupying it.
+	let spaceInBetween = null;
+
+	//if the attempted move is a jump
+	if(square - movingPiece > 10 || movingPiece - square > 10){
+
+		const madeMove = possibleMoves.indexOf(square);
+
+		let spaceCount;
+
+		//destination 'square' must be a possible move of moving piece
+		switch(madeMove){
+
+			case 0:
+			case 1:
+				spaceCount = (movingPiece - square) / 2;
+				spaceInBetween = movingPiece - spaceCount;
+				break;
+
+			case 2:
+			case 3:
+				spaceCount = (square - movingPiece) / 2;
+				spaceInBetween = movingPiece + spaceCount;
+				break;
+
+			default:
+				return [null, null];
 		}
-		for (let i = 0; i < redOverBlack.length; ++i){
-			const [moving, left, right] = redOverBlack[i];
-			if(moving === movingPiece){ 
-				if (left === square && (squares[left + 9] === "O" || squares[left + 9] === 'OK') && squares[left] === null) 
-						return [true, left + 9];
-				if (right === square && (squares[right + 7] === 'O' || squares[right + 7] === 'OK') && squares[right] === null)
-						return [true, right + 7];
-			}
+
+		//the piece in bteween must be an opponent piece
+		switch (turn){
+			case 'X':
+			case 'XK':
+				if(blackPieces.includes(squares[spaceInBetween]))
+					break;
+
+			case 'O':
+			case 'OK':
+				if(redPieces.includes(squares[spaceInBetween]))
+					break;
+
+			default:
+				return [null, null];
+
 		}
 
 	}
 
-	else if(turn === 'XK' || turn === 'OK'){
-		//moving down the board
-		if(square > movingPiece){
-			for( let i = 0; i < blackAllowed.length; ++i){
-				const [moving, left, right] = blackAllowed[i];
-
-				if(moving === movingPiece && ((left === square && squares[left] === null) || (right === square && squares[right] === null)) ){
-					if (jump)
-						return [null, null];
-					return [true, null];
-				}
-			}
-
-			for( let i = 0; i < blackOverRed.length; ++i){
-				const [moving, left, right] = blackOverRed[i];
-				if (turn === 'XK'){
-					if(moving === movingPiece){
-
-						if(square === left){
-							if((squares[left - 7] === 'O' || squares[left - 7] === 'OK') && squares[left] === null)
-								return [true, left - 7];
-						}
-
-						else if (square === right){
-							if((squares[right - 9] === 'O' || squares[right - 9] === 'OK') && squares[right] === null)
-								return [true, right - 9];
-						}
-					}
-				}
-				else if (turn === 'OK'){
-					if(moving === movingPiece){
-						if(square === left){
-
-							if((squares[left - 7] === 'X' || squares[left - 7] === 'XK') && squares[left] === null)
-								return [true, left - 7];
-
-						}
-						else if (square === right){
-							if((squares[right - 9] === 'X' || squares[right - 9] === 'XK') && squares[right] === null)
-								return [true, right - 9];
-						}
-					}
-				}
-			}
-		}
-
-		//moving up the board
-		else if (square < movingPiece){
-			for( let i = 0; i < redAllowed.length; ++i){
-				const [moving, left, right] = redAllowed[i];
-				if(moving === movingPiece && ((left === square && squares[left] === null) || (right === square && squares[right] === null)) ){
-					if (jump)
-						return [null, null];
-					return [true, null];
-				}
-			}
-
-			for( let i = 0; i < redOverBlack.length; ++i){
-				const [moving, left, right] = redOverBlack[i];
-				if (turn === 'XK'){
-					if(moving === movingPiece){
-
-						if(square === left){
-							if((squares[left + 9] === 'O' || squares[left + 9] === 'OK') && squares[left] === null)
-								return [true, left + 9];
-						}
-
-						else if (square === right){
-							if((squares[right + 7] === 'O' || squares[right + 7] === 'OK') && squares[right] === null)
-								return [true, right + 7];
-						}
-					}
-				}
-				else if (turn === 'OK'){
-					if(moving === movingPiece){
-						if(square === left){
-
-							if((squares[left + 9] === 'X' || squares[left + 9] === 'XK') && squares[left] === null)
-								return [true, left + 9];
-
-						}
-						else if (square === right){
-							if((squares[right + 7] === 'X' || squares[right + 7] === 'XK') && squares[right] === null)
-								return [true, right + 7];
-						}
-					}
-				}
-			}
-		}
+	if(jump && !spaceInBetween){
+		return [null, null];
 	}
 
+	//if moving piece is on the left edge of board, only allows right moves [1] [3]
+	if(leftEdge.includes(movingPiece) && (possibleMoves[1] === square || possibleMoves[3] === square))
+		return [true, spaceInBetween];
+
+	//if moving piece is on the right edge of board, only allows left moves [0] [2]
+	if(rightEdge.includes(movingPiece) && (possibleMoves[0] === square || possibleMoves[2] === square))
+		return [true, spaceInBetween];
+
+	//else piece can move right or left
+	if(possibleMoves.includes(square))
+		return [true, spaceInBetween];
 
 	return [null, null];
 }
